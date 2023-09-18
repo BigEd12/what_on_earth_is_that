1170142cdf5b5ba354336e6d88442b4a966ed68c
Eduardo Olmos <olmos.edward@gmail.com>
2023-09-18 09:47:28 +0200

    flow changes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from utils.sourcing import extract_plant_data
 from utils.page_dict import plant_dict
 
-extract_plant_data(plant_dict["Astragaluses"], "Astragaluses")
+
+
+#extract_plant_data(plant_dict["Astragaluses"], "Astragaluses")
+for item, value in plant_dict.items():
+    if item == 'Yarrows':
+        print(f"Currently extracting {item}")
+        extract_plant_data(value, item)
